eb914a8842b28e3f96e5fecddefb8af70014f921
st <st>
2023-11-14 18:23:01 +0300

    add todos

--- src/QA.Telegram.Bot.App/Program.cs
+++ src/QA.Telegram.Bot.App/Program.cs
@@ -10,6 +10,7 @@ IHost host = Host.CreateDefaultBuilder(args)
     {
         services.AddDbContext<QaContext>(opt =>
             opt.UseSqlServer(
+                //todo move to config
                 "Server=localhost;initial catalog=QA_DB;user ID=sa;Password=yourStrong(!)Password;TrustServerCertificate=True"));
         services.AddHostedService<BotService>();
         services.AddScoped<IQaRepo, SqlQaRepo>();
