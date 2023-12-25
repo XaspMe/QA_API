63af42aaf8a9401d7a9667d03fa21ea28bd3beda
st <st>
2023-12-25 18:09:56 +0300

    add access time;

---
+++ src/QA.Data/Migrations/20231225150912_add_access_time.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QA.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_access_time : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastAccess",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastAccess",
+                table: "Users");
+        }
+    }
+}
